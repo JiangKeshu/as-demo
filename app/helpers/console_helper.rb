c6bace26458c58d6d829538fe3ecf68804b931a0
EC2 Default User <ec2-user@ip-172-31-3-194.ap-southeast-1.compute.internal>
2014-04-29 14:45:10 +0000

    init

---
+++ app/helpers/console_helper.rb
@@ -0,0 +1,7 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+Asdemo::App.helpers do
+  # def simple_helper_method
+  #  ...
+  # end
+end
---
+++ config/apps.rb
@@ -0,0 +1,36 @@
+##
+# This file mounts each app in the Padrino project to a specified sub-uri.
+# You can mount additional applications using any of these commands below:
+#
+#   Padrino.mount('blog').to('/blog')
+#   Padrino.mount('blog', :app_class => 'BlogApp').to('/blog')
+#   Padrino.mount('blog', :app_file =>  'path/to/blog/app.rb').to('/blog')
+#
+# You can also map apps to a specified host:
+#
+#   Padrino.mount('Admin').host('admin.example.org')
+#   Padrino.mount('WebSite').host(/.*\.?example.org/)
+#   Padrino.mount('Foo').to('/foo').host('bar.example.org')
+#
+# Note 1: Mounted apps (by default) should be placed into the project root at '/app_name'.
+# Note 2: If you use the host matching remember to respect the order of the rules.
+#
+# By default, this file mounts the primary app which was generated with this project.
+# However, the mounted app can be modified as needed:
+#
+#   Padrino.mount('AppName', :app_file => 'path/to/file', :app_class => 'BlogApp').to('/')
+#
+
+##
+# Setup global project settings for your apps. These settings are inherited by every subapp. You can
+# override these settings in the subapps as needed.
+#
+Padrino.configure_apps do
+  # enable :sessions
+  set :session_secret, '99dfe88ff7c18c737facf9ac6b90561f336d41b32d653f062663bce11c5c4443'
+  set :protection, :except => :path_traversal
+  set :protect_from_csrf, true
+end
+
+# Mounts the core application for this project
+Padrino.mount('Asdemo::App', :app_file => Padrino.root('app/app.rb')).to('/')
